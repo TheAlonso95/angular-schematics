86926ed910efe875a4c063851f35a7067ee32ac3
Alonso <rafael.a.m95@hotmail.com>
2021-08-20 19:52:36 +0000

    Test a simple schematic

--- master-detail/src/master-detail/index_spec.ts
+++ master-detail/src/master-detail/index_spec.ts
@@ -1,16 +1,59 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {
+	SchematicTestRunner,
+	UnitTestTree,
+} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-const collectionPath = path.join(__dirname, '../collection.json');
-
 describe('master-detail', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner
-      .runSchematicAsync('master-detail', {}, Tree.empty())
-      .toPromise();
+	const schematicRunner = new SchematicTestRunner(
+		'schematics',
+		path.join(__dirname, './../collection.json')
+	);
 
-    expect(tree.files).toEqual([]);
-  });
+	const workspaceOptions: Record<string, string> = {
+		name: 'workspace',
+		newProjectRoot: 'projects',
+		version: '0.5.0',
+	};
+
+	const appOptions: Record<string, string> = {
+		name: 'schematest',
+	};
+
+	const schemaOptions: Record<string, string> = {
+		name: 'foo',
+	};
+
+	let appTree: UnitTestTree;
+
+	beforeEach(async () => {
+		appTree = await schematicRunner
+			.runExternalSchematicAsync(
+				'@schematics/angular',
+				'workspace',
+				workspaceOptions
+			)
+			.toPromise();
+		appTree = await schematicRunner
+			.runExternalSchematicAsync(
+				'@schematics/angular',
+				'application',
+				appOptions,
+				appTree
+			)
+			.toPromise();
+	});
+
+	it('works', (done) => {
+		schematicRunner
+			.runSchematicAsync('master-detail', schemaOptions, appTree)
+			.toPromise()
+			.then((tree) => {
+				const appComponent = tree.readContent(
+					'/projects/schematest/src/app/components/master.component.ts'
+				);
+				expect(appComponent).toContain(`name = '${schemaOptions.name}'`);
+				done();
+			}, done.fail);
+	});
 });
